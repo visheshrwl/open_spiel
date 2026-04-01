b375c51d897f9958a1dce6d7463f8563ec667e62
Arahan-kujur <kujurarahan@gmail.com>
2026-04-01 16:22:22 +0530

    Trigger CI tests

--- open_spiel/games/xiangqi/xiangqi.h
+++ open_spiel/games/xiangqi/xiangqi.h
@@ -11,6 +11,8 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+// Minor change to trigger CI rerun
+
 
 #ifndef OPEN_SPIEL_GAMES_XIANGQI_H_
 #define OPEN_SPIEL_GAMES_XIANGQI_H_
